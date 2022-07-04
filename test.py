313975e621f4dc7ccab207b813f864ef5c9df311
lenovo <1661849515@qq.com>
2022-07-04 21:46:55 +0800

    conflict fixed1

--- test.py
+++ test.py
@@ -5,9 +5,5 @@ print(1 < 2)
 # Git is free software distributed under the GPL.
 # Git has a mutable index called stage.
 # Git tracks changes of files.
-<<<<<<< HEAD
 # Creating a new branch is quick AND simple.
 
-=======
-# Creating a new branch is quick AND simple.
->>>>>>> feature1
